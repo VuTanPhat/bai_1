f6847e45d8112f1f7f2e239f420d436a4bf8c9b7
VuTanPhat <124854669+VuTanPhat@users.noreply.github.com>
2023-03-28 13:13:54 +0700

    Add files via upload

---
+++ Bai1.2.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+struct HonSo
+{
+    int Tu;
+	int Mau;
+	int Nguyen;
+};
+typedef struct HonSo HONNSO;
+void NhapHonSo (HONNSO &);
+void XuatHonSo (HONSO);
+void NhapHonSo (HONSO &hs)
+{	
+	printf("Nhap nguyen: ");
+    scanf("%d", &hs.Nguyen);
+    printf("Nhap tu: ");
+    scanf("%d", &hs.Tu);
+    do
+    {
+        printf("Nhap mau: ");
+        scanf("%d", &hs.Mau);
+        if(hs.Mau== 0)
+        {
+            printf("Mau so phai khac 0");
+        }
+    }while(hs.Mau== 0);
+}
+void XuatHonSo(HONSO hs)
+{
+    printf("%d %d/%d",hs.Nguyen,hs.Tu, hs.Mau);
+}
+int main()
+{
+    HONSO hs;
+    NhapHonSo(hs);
+    XuatHonSo(hs);
+    getch();
+    return 0;
+}
